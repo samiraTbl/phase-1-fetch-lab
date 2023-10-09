82e629e36809ebda28c7b61efb6e78e1227f364f
samira toubal seghir <samsidt@gmail.com>
2023-10-09 02:24:12 +0200

    complited assignemnt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
+  renderBooks(json)
 }
 
 function renderBooks(books) {
@@ -14,4 +16,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  
 });
